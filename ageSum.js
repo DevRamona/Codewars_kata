1c00d6d25502b115a3537b31d7ef2a1712e2b895
DevRamona <r.ingabire@alustudent.com>
2024-05-24 22:22:36 +0200

    done solving the sum of the age

--- ageSum.js
+++ ageSum.js
@@ -1,12 +1,12 @@
 // You are given an array of objects representing employees in a company. Each object contains the following properties: name, age, and department. Your task is to write a function to calculate the average age of each department's employees. The function should return an object containing the department name and the summation of employees' age in each department.
 // //Example Input:
-// const employees = [
-// { name: 'Alice', age: 30, department: 'Engineering' },
-// { name: 'Bob', age: 25, department: 'Marketing' },
-// { name: 'Charlie', age: 35, department: 'Engineering' },
-// { name: 'David', age: 40, department: 'HR' },
-// { name: 'Eve', age: 28, department: 'Marketing' },
-// ];
+const employees = [
+ { name: 'Alice', age: 30, department: 'Engineering' },
+ { name: 'Bob', age: 25, department: 'Marketing' },
+ { name: 'Charlie', age: 35, department: 'Engineering' },
+ { name: 'David', age: 40, department: 'HR' },
+ { name: 'Eve', age: 28, department: 'Marketing' },
+ ];
 
 // output:
 // {
@@ -15,9 +15,13 @@
 // HR: 40
 
 
-const arrayObject = (employees) {
-    newArr = {}
-    for(const employee of employees) {
-        const {department, age} = employees
+const arrayObject = (employees) => {
+    newArr = {};
+    employees.forEach(({age, department}) => {
+        newArr[department] = (newArr[department] || 0) + age;
+    });
+    return newArr;
+    
     }
-}
+
+console.log(arrayObject(employees))
